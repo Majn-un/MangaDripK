f513801fa9660f89933ad82b3f5494ff2dca071c
abdulahi <abulahibas@gmail.com>
2020-08-08 17:19:16 -0500

    Classes

---
+++ app/src/main/java/com/example/mangadripk/Classes/Page.kt
@@ -0,0 +1,13 @@
+package com.example.mangadripk.Classes
+
+class Page {
+    var link: String? = null
+    var name: String? = null
+
+    constructor() {}
+    constructor(link: String?, name: String?) {
+        this.link = link
+        this.name = name
+    }
+
+}
