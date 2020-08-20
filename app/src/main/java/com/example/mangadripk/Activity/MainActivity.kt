6bb7106e277c91cfff0d3813f6e39f48cb62d817
abdulahi <abulahibas@gmail.com>
2020-08-19 21:59:42 -0500

    toolbar

--- app/src/main/java/com/example/mangadripk/Activity/MainActivity.kt
+++ app/src/main/java/com/example/mangadripk/Activity/MainActivity.kt
@@ -7,7 +7,9 @@ import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.example.mangadripk.Fragments.Download
 import com.example.mangadripk.Fragments.Favorite
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -16,16 +18,28 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.example.mangadripk.Fragments.Library
 import com.example.mangadripk.Fragments.Recent
 import com.example.mangadripk.R
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setOnNavigationItemSelectedListener(navListener)
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, Library()).commit()
+        //toolbar
+//        val mtoolbar = findViewById<Toolbar>(R.id.toolbar)
+//        val mtabitemAll = findViewById<TabLayout>(R.id.All)
+//        val mtabitemLatest=findViewById<TabItem>(R.id.Latest)
+//        val mTabLayout = findViewById<TabLayout>(R.id.Tablayout)
+//        setSupportActionBar(mtoolbar)
+
     }
 
     private val navListener =
---
+++ app/src/main/java/com/example/mangadripk/Fragments/Latest.kt
@@ -0,0 +1,4 @@
+package com.example.mangadripk.Fragments
+
+class Latest {
+}
