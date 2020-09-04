1a84695e704a91d9ef3f8eee5817853ce1682c52
Mohammed Hager <mohager154@gmail.com>
2020-09-03 22:00:34 -0500

    phase 1 of search is working

--- app/src/main/java/com/example/mangadripk/Fragments/FragAll.kt
+++ app/src/main/java/com/example/mangadripk/Fragments/FragAll.kt
@@ -3,7 +3,6 @@ package com.example.mangadripk.Fragments
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.*
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
@@ -14,23 +13,29 @@ import com.example.mangadripk.Adapter.RecyclerViewAdapter
 import com.example.mangadripk.Classes.CustomProgressDialog
 import com.example.mangadripk.R
 import com.example.mangadripk.Sources.Sources
+import com.example.mangadripk.Sources.manga.MangaHere
 import com.google.android.material.tabs.TabLayout
 import com.programmersbox.manga_sources.mangasources.MangaModel
 import com.programmersbox.mangaworld.views.Utility
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okhttp3.CacheControl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.jsoup.Jsoup
+import java.util.concurrent.TimeUnit
 
 
 class FragAll : Fragment() {
     private var myAdapter: RecyclerViewAdapter? = null
     private val mangaList = mutableListOf<MangaModel>()
-
     private val searchList = mutableListOf<MangaModel>()
     private val test = mutableListOf<MangaModel>()
     private val progressDialog = CustomProgressDialog()
     private var pageNumber = 1
     private val baseUrl = "https://www.mangahere.cc"
-    lateinit var gridlayoutManager : GridLayoutManager
+    lateinit var gridlayoutManager: GridLayoutManager
 
 
     var myFragment: View? = null
@@ -55,13 +60,8 @@ class FragAll : Fragment() {
         // Inflate the layout for this fragment
 
 
-
-
-
-
-
         setHasOptionsMenu(true)
-        val view : View = inflater.inflate(R.layout.fragment_all, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_all, container, false)
 
 
 
@@ -77,7 +77,8 @@ class FragAll : Fragment() {
 
         val myrv = view.findViewById(R.id.all_id) as RecyclerView
         myAdapter = activity?.let { RecyclerViewAdapter(it, mangaList) }
-        gridlayoutManager = Utility(activity, 400).apply { orientation = GridLayoutManager.VERTICAL }
+        gridlayoutManager =
+            Utility(activity, 400).apply { orientation = GridLayoutManager.VERTICAL }
         myrv.layoutManager = gridlayoutManager
         myrv.adapter = myAdapter
         myrv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
@@ -121,10 +122,56 @@ class FragAll : Fragment() {
                         newList.add(manga)
                     }
                     myAdapter?.setFilter(newList)
+                    if (newList.isEmpty()) {
+//                        activity?.let { progressDialog.show(it) }
+                        SearchMangaHere(newText)
+                        myAdapter?.setFilter(newList)
+
+                    }
                     return false
                 }
             }
         searchView.setOnQueryTextListener(queryTextListener)
+    }
+
+    private fun SearchMangaHere(search_item: String) {
+        val closeActivity = Thread {
+            try {
+                Thread.sleep(3000)
+                // Do some stuff
+            } catch (e: java.lang.Exception) {
+                e.localizedMessage
+            }
+        }
+        GlobalScope.launch {
+            try {
+                delay(500)
+                val desiredManga = search_item.replace(" ", "+")
+                val url =
+                    "https://www.mangahere.cc/search?title=$desiredManga&genres=&nogenres=&sort=&stype=1&name=&type=0&author_method=cw&author=&artist_method=cw&artist=&rating_method=eq&rating=&released_method=eq&released=&st=0"
+                val response = Jsoup.connect(url).get().select(".manga-list-4-list > li")
+                    .map {
+                        MangaModel(
+                            title = it.select("a").first().attr("title"),
+                            description = it.select("p.manga-list-4-item-tip").last().text(),
+                            mangaUrl = "${baseUrl}${
+                                it.select(".manga-list-4-item-title > a")
+                                    .first().attr("href")
+                            }",
+                            imageUrl = it.select("img.manga-list-4-cover").first().attr("abs:src"),
+                            source = Sources.MANGA_HERE
+                        )
+                    }
+
+                for (item in response) {
+                    mangaList.add(item)
+                }
+
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun loadNewManga() {
